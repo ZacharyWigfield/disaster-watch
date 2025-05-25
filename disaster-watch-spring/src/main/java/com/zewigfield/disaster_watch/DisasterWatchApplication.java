3c8735bd3f5b6246f0af3acb6800c83ced6bc157
Zachary <zewigfield@gmail.com>
2025-05-25 15:14:13 -0400

    initial commit

---
+++ disaster-watch-spring/src/main/java/com/zewigfield/disaster_watch/DisasterWatchApplication.java
@@ -0,0 +1,15 @@
+package com.zewigfield.disaster_watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterWatchApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DisasterWatchApplication.class, args);
+
+    }
+
+}
---
+++ disaster-watch-spring/src/test/java/com/zewigfield/disaster_watch/DisasterWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zewigfield.disaster_watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
