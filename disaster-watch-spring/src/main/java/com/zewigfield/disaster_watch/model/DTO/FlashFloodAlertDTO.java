cb373ce573b73bcb13e55067fed0480d73b210f6
Zachary <zewigfield@gmail.com>
2025-05-30 06:11:30 -0400

    changed flash flood data object to return a list of strings instead of a single string

--- disaster-watch-spring/src/main/java/com/zewigfield/disaster_watch/model/DTO/FlashFloodAlertDTO.java
+++ disaster-watch-spring/src/main/java/com/zewigfield/disaster_watch/model/DTO/FlashFloodAlertDTO.java
@@ -3,10 +3,12 @@ package com.zewigfield.disaster_watch.model.DTO;
 import com.zewigfield.disaster_watch.model.entity.FlashFloodAlert;
 
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
 
 public class FlashFloodAlertDTO {
     private final String id;
-    private final String areaDesc;
+    private final List<String> areaDesc;
     private final String event;
     private final String description;
     private final Instant effective;
@@ -14,15 +16,19 @@ public class FlashFloodAlertDTO {
 
     public FlashFloodAlertDTO(FlashFloodAlert alert) {
         this.id = alert.getId();
-        this.areaDesc = alert.getAreaDesc();
+        this.areaDesc = parseAreaDesc(alert.getAreaDesc());
         this.event = alert.getEvent();
         this.description = alert.getDescription();
         this.effective = alert.getEffective();
         this.expires = alert.getExpires();
     }
 
+    private List<String> parseAreaDesc (String areaDesc){
+        return Arrays.stream(areaDesc.split(";")).map(String::trim).toList();
+    }
+
     public String getId() { return id; }
-    public String getAreaDesc() { return areaDesc; }
+    public List<String> getAreaDesc() { return areaDesc; }
     public String getEvent() { return event; }
     public String getDescription() { return description; }
     public Instant getEffective() { return effective; }
