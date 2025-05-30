c02649fa40ecb1347a92a88d480679bec34a56e6
Zachary <zewigfield@gmail.com>
2025-05-30 05:43:36 -0400

    added a pruning function so that alerts older than 30 days are removed from the db table

--- disaster-watch-spring/src/main/java/com/zewigfield/disaster_watch/AppStartupRunner.java
+++ disaster-watch-spring/src/main/java/com/zewigfield/disaster_watch/AppStartupRunner.java
@@ -1,20 +1,32 @@
 package com.zewigfield.disaster_watch;
 
+import com.zewigfield.disaster_watch.repository.FlashFloodAlertRepository;
 import com.zewigfield.disaster_watch.service.FlashFloodAlertService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 @Component
 public class AppStartupRunner implements CommandLineRunner {
 
     private final FlashFloodAlertService service;
+    private final FlashFloodAlertRepository repository;
 
-    public AppStartupRunner(FlashFloodAlertService service) {
+    public AppStartupRunner(FlashFloodAlertService service, FlashFloodAlertRepository repository) {
         this.service = service;
+        this.repository = repository;
     }
 
     @Override
     public void run(String... args) {
+        pruneOldAlerts();
         service.fetchAndStoreAlerts();
+    }
+
+    public void pruneOldAlerts() {
+        Instant cutoff = Instant.now().minus(30, ChronoUnit.DAYS);
+        repository.deleteByEffectiveBefore(cutoff);
     }
 }
--- disaster-watch-spring/src/main/java/com/zewigfield/disaster_watch/repository/FlashFloodAlertRepository.java
+++ disaster-watch-spring/src/main/java/com/zewigfield/disaster_watch/repository/FlashFloodAlertRepository.java
@@ -2,6 +2,14 @@ package com.zewigfield.disaster_watch.repository;
 
 import com.zewigfield.disaster_watch.model.entity.FlashFloodAlert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.Instant;
 
 public interface FlashFloodAlertRepository extends JpaRepository<FlashFloodAlert, String> {
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM FlashFloodAlert f WHERE f.effective < :cutoff")
+    int deleteByEffectiveBefore(Instant cutoff);
 }
