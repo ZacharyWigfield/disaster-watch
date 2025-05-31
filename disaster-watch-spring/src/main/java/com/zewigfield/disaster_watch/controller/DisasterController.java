15cd3cf47d8974a9820eb5b41697ee89a3fd7885
Zachary <zewigfield@gmail.com>
2025-05-31 03:35:21 -0400

    set query params for flood/warnings mapping

--- disaster-watch-spring/src/main/java/com/zewigfield/disaster_watch/controller/DisasterController.java
+++ disaster-watch-spring/src/main/java/com/zewigfield/disaster_watch/controller/DisasterController.java
@@ -2,8 +2,10 @@ package com.zewigfield.disaster_watch.controller;
 
 import com.zewigfield.disaster_watch.model.DTO.FloodAlertDTO;
 import com.zewigfield.disaster_watch.repository.FloodAlertRepository;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.List;
 
 @RestController
@@ -20,7 +22,13 @@ public class DisasterController {
 
     // returns flood and flash flood warnings of extreme and severe severity and observed and likely certainty
     @GetMapping("/floods/warnings")
-    public List<FloodAlertDTO> getFloodWarnings() {
+    public List<FloodAlertDTO> getFloodWarnings(
+            @RequestParam String search,
+            @RequestParam int radius,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant startDate,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant endDate,
+            @RequestParam List<String> disasterType
+    ) {
         return floodAlertRepository.findAll().stream()
                 .map(FloodAlertDTO::new)
                 .toList();
