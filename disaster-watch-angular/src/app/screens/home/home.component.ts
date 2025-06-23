3190164d59caadddcca5bf58f13464a6f6a63e73
Zachary <zewigfield@gmail.com>
2025-06-23 07:31:34 -0400

    adjusted home component to use ngSrc and optimized image

--- disaster-watch-angular/src/app/screens/home/home.component.ts
+++ disaster-watch-angular/src/app/screens/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from '../../components/search/search.component';
 import { DisasterListComponent } from '../../components/disaster-list/disaster-list.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [SearchComponent, DisasterListComponent,],
+  imports: [SearchComponent, DisasterListComponent, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
