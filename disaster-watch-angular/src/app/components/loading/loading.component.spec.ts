127f194191f592c17e720410b8d2a62f0fd73adf
Zachary <zewigfield@gmail.com>
2025-07-05 06:04:10 -0400

    created unit tests for loading component

--- disaster-watch-angular/src/app/components/loading/loading.component.spec.ts
+++ disaster-watch-angular/src/app/components/loading/loading.component.spec.ts
@@ -1,23 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoadingComponent } from './loading.component';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [LoadingComponent],
+  template: `<app-loading [loading]="isLoading" />`
+})
+class HostComponent {
+  isLoading = false;
+}
 
 describe('LoadingComponent', () => {
-  let component: LoadingComponent;
-  let fixture: ComponentFixture<LoadingComponent>;
+  let component: HostComponent;
+  let fixture: ComponentFixture<HostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoadingComponent]
+      imports: [HostComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
-    fixture = TestBed.createComponent(LoadingComponent);
+    fixture = TestBed.createComponent(HostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show loading message when loading is true', () => {
+    component.isLoading = true;
+    fixture.detectChanges();
+
+    const spinner = fixture.nativeElement.querySelector('#loading-spinner');
+    expect(spinner).toBeTruthy();
+  });
+
+  it('should not show anything when loading is false', () => {
+    component.isLoading = false;
+    fixture.detectChanges();
+
+    const spinner = fixture.nativeElement.querySelector('#loading-spinner');
+    expect(spinner).toBeFalsy();
   });
 });
